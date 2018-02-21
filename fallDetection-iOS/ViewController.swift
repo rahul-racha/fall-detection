848bdadd7657273c510a555e7f4802f38c0a625d
Rahul Racha <rahulracha@rahuls-macbook-pro.local>
2018-02-21 11:30:09 -0500

    Initial Commit

---
+++ fallDetection-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fallDetection-iOS
+//
+//  Created by Rahul Racha on 2/21/18.
+//  Copyright © 2018 Rahul Racha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ fallDetection-iOSTests/fallDetection_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  fallDetection_iOSTests.swift
+//  fallDetection-iOSTests
+//
+//  Created by Rahul Racha on 2/21/18.
+//  Copyright © 2018 Rahul Racha. All rights reserved.
+//
+
+import XCTest
+@testable import fallDetection_iOS
+
+class fallDetection_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
